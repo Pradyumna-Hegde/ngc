42c1d35f17b633b5468fa747d8a8b5a13d56152f
Pradyumna-Hegde <pradyumna.coder@gmail.com>
2023-10-01 19:39:08 +0530

    added more source codes to the <WaitlistForm /> component

---
+++ src/components/StaticValues.ts
@@ -0,0 +1,15 @@
+const genderList = ["Male", "Female", "Other"];
+
+const answerList = ["To Learn with neoG community", "To get a job in 2024"];
+
+const backgroundStatus = [
+  "School Student",
+  "College Student",
+  "Working Professional (Web-dev)",
+  "Graduate but not working yet",
+  "Working Professional (other sector)",
+  "Not working currently",
+  "other",
+];
+
+export { genderList, answerList, backgroundStatus };
--- src/components/WaitlistForm.tsx
+++ src/components/WaitlistForm.tsx
@@ -1,27 +1,173 @@
 import { FormEvent, useState } from "react";
+import { genderList, answerList, backgroundStatus } from "./StaticValues.ts";
 
 const WaitlistForm = () => {
-  const [fullName, setFullName] = useState("");
+  const [person, setPerson] = useState({
+    fullName: "",
+    gender: "",
+    phone: "",
+    email: "",
+    qna: "",
+    status: "",
+    graduationYear: "",
+  });
 
   const handleSubmit = (event: FormEvent) => {
     event.preventDefault();
-    console.log(fullName);
+    console.log(person);
   };
   return (
     <form onSubmit={handleSubmit}>
+      {/* Full Name Input Field */}
       <div className="mb-3">
         <label htmlFor="full-name" className="form-label">
           Full Name
         </label>
         <input
-          onChange={(event) => setFullName(event?.target.value)}
-          value={fullName}
+          onChange={(event) =>
+            setPerson({ ...person, fullName: event?.target.value })
+          }
+          value={person.fullName}
           type="text"
           id="full-name"
           className="form-control"
           autoComplete="full-name"
         />
+        <p className="text-secondary" style={{ fontSize: "12px" }}>
+          Please fill out your official name here. Please do not provide your
+          Discord or social media name *
+        </p>
       </div>
+
+      {/* Gender Input Field */}
+      <div className="mb-3">
+        <label htmlFor="gender" className="form-label">
+          Gender
+        </label>
+        <select
+          autoComplete="gender"
+          id="gender"
+          className="form-select"
+          onChange={(event) =>
+            setPerson({ ...person, gender: event?.target.value })
+          }
+        >
+          <option defaultValue={"Select Gender"}>Select gender</option>
+          {genderList.map((gender) => (
+            <option key={gender} value={gender}>
+              {gender}
+            </option>
+          ))}
+        </select>
+      </div>
+
+      {/* Phone Number Input Field */}
+      <div className="mb-3">
+        <label htmlFor="phone" className="form-label">
+          Phone Number
+        </label>
+        <input
+          onChange={(event) =>
+            setPerson({ ...person, phone: event?.target.value })
+          }
+          value={person.phone}
+          type="tel"
+          id="phone"
+          className="form-control"
+          autoComplete="phone"
+        />
+        <p className="text-secondary" style={{ fontSize: "12px" }}>
+          active phone number *
+        </p>
+      </div>
+
+      {/* Email Input Field */}
+      <div className="mb-3">
+        <label htmlFor="email" className="form-label">
+          Email
+        </label>
+        <input
+          onChange={(event) =>
+            setPerson({ ...person, email: event?.target.value })
+          }
+          type="email"
+          id="email"
+          value={person.email}
+          autoComplete="email"
+          className="form-control"
+        />
+        <p className="text-secondary" style={{ fontSize: "12px" }}>
+          please share your active email address *
+        </p>
+      </div>
+
+      {/* Why Input Field */}
+      <div className="mb-3">
+        <label htmlFor="qna" className="form-label">
+          Why do you want to join the camp?
+        </label>
+        <select
+          autoComplete="qna"
+          id="qna"
+          className="form-select"
+          onChange={(event) =>
+            setPerson({ ...person, qna: event?.target.value })
+          }
+        >
+          <option defaultValue={"Select"}>Select</option>
+          {answerList.map((answer) => (
+            <option key={answer} value={answer}>
+              {answer}
+            </option>
+          ))}
+        </select>
+      </div>
+
+      {/* current educational/professional status Input Field */}
+      <div className="mb-3">
+        <label htmlFor="status" className="form-label">
+          What is your current educational/professional status?
+        </label>
+        <select
+          autoComplete="status"
+          id="status"
+          className="form-select"
+          onChange={(event) =>
+            setPerson({ ...person, status: event?.target.value })
+          }
+        >
+          <option defaultValue={"Select Status"}>Select Status</option>
+          {backgroundStatus.map((answer) => (
+            <option key={answer} value={answer}>
+              {answer}
+            </option>
+          ))}
+        </select>
+      </div>
+
+      {/* Year of Graduation Input Field */}
+      <div className="mb-3">
+        <label htmlFor="graduation-year" className="form-label">
+          Year of Graduation
+        </label>
+        <input
+          onChange={(event) =>
+            setPerson({ ...person, graduationYear: event?.target.value })
+          }
+          value={person.graduationYear}
+          type="number"
+          id="graduation-year"
+          className="form-control"
+          autoComplete="graduation-year"
+        />
+        <p className="text-secondary" style={{ fontSize: "12px" }}>
+          Your degree does not define your success. We need it to prepare you
+          better. Please write your year of graduation (latest degree). eg:
+          2023. Please write 1111 if you have not been to college or are a drop
+          out.*
+        </p>
+      </div>
+
       <button type="submit" className="btn btn-primary">
         Submit
       </button>
